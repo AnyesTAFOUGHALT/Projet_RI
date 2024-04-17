d2b99ece3f4b90554994c34f343507fda945d5aa
racha1004 <rachanadine2002@gmail.com>
2024-04-17 13:15:59 +0200

    RandomcharSUb

---
+++ Misspelling.py
@@ -0,0 +1,26 @@
+import random
+import string
+
+def RemoveStopWords(query):
+    """s Removes all stopwords from the query."""
+    pass
+def RandomCharSub (query):
+    """
+    Replaces a random character from a random query
+    term (excluding stopwords) with a randomly chosen new ASCII
+    character.
+    """
+    query_cleaned = (query)
+
+    query_splitted = query_cleaned.split()
+
+    random_term = random.choice(query_splitted)
+    random_char = random.choice(range(len(random_term)))
+
+    new_char= random.choice(string.ascii_letters)
+
+    modified_term = random_term[:random_char] + new_char + random_term[random_char + 1:]
+    modified_query = query.replace(random_term, modified_term)
+
+    return modified_query
+
