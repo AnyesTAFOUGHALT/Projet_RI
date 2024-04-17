c2bb876c66d4717e6585d7f460eee87abc086bcd
AnyesTAFOUGHALT <anyestafoughalt@gmail.com>
2024-04-17 13:11:10 +0200

    RemoveStopWords

--- Naturality.py
+++ Naturality.py
@@ -1,2 +1,15 @@
-def RemoveStopWords(query) :
-    return query
+import nltk
+from nltk.corpus import stopwords
+nltk.download('stopwords')
+
+def RemoveStopWords(query):
+    """
+    Removes all stopwords from the query
+    """
+    query_lower = query.lower()
+    tokens = nltk.word_tokenize(query_lower)
+    query_without_stop_words = [word for word in tokens if word not in stopwords.words('english')]
+    query_without_stop_words = ' '.join(query_without_stop_words)
+    return query_without_stop_words
+
+print(RemoveStopWords("What is durable Medical equipment Consist of"))
